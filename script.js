20190dabfc5ab79f111a01f0da758c9e83d9acc6
gMSchw <guillermoschw@gmail.com>
2022-09-23 20:06:06 -0300

    Finished Calculator interface

--- script.js
+++ script.js
@@ -30,80 +30,80 @@ function operate(a, operator, b) {
     }
 }
 
-let display = document.querySelector('#display');
+let cuentaContainer = document.querySelector('#cuentaContainer');
 
 const btnZero = document.querySelector('#btnZero');
 btnZero.addEventListener('click', () => {
-    display.textContent = display.textContent + '0';
+    cuentaContainer.textContent = cuentaContainer.textContent + '0';
 })
 const btnOne = document.querySelector('#btnOne');
 btnOne.addEventListener('click', () => {
-    display.textContent = display.textContent + '1';
+    cuentaContainer.textContent = cuentaContainer.textContent + '1';
 })
 const btnTwo = document.querySelector('#btnTwo');
 btnTwo.addEventListener('click', () => {
-    display.textContent = display.textContent + '2';
+    cuentaContainer.textContent = cuentaContainer.textContent + '2';
 })
 const btnThree = document.querySelector('#btnThree');
 btnThree.addEventListener('click', () => {
-    display.textContent = display.textContent + '3';
+    cuentaContainer.textContent = cuentaContainer.textContent + '3';
 })
 const btnFour = document.querySelector('#btnFour');
 btnFour.addEventListener('click', () => {
-    display.textContent = display.textContent + '4';
+    cuentaContainer.textContent = cuentaContainer.textContent + '4';
 })
 const btnFive = document.querySelector('#btnFive');
 btnFive.addEventListener('click', () => {
-    display.textContent = display.textContent + '5';
+    cuentaContainer.textContent = cuentaContainer.textContent + '5';
 })
 const btnSix = document.querySelector('#btnSix');
 btnSix.addEventListener('click', () => {
-    display.textContent = display.textContent + '6';
+    cuentaContainer.textContent = cuentaContainer.textContent + '6';
 })
 const btnSeven = document.querySelector('#btnSeven');
 btnSeven.addEventListener('click', () => {
-    display.textContent = display.textContent + '7';
+    cuentaContainer.textContent = cuentaContainer.textContent + '7';
 })
 const btnEight = document.querySelector('#btnEight');
 btnEight.addEventListener('click', () => {
-    display.textContent = display.textContent + '8';
+    cuentaContainer.textContent = cuentaContainer.textContent + '8';
 })
 const btnNine = document.querySelector('#btnNine');
 btnNine.addEventListener('click', () => {
-    display.textContent = display.textContent + '9';
+    cuentaContainer.textContent = cuentaContainer.textContent + '9';
 })
 const btnDivide = document.querySelector('#btnDivide');
 btnDivide.addEventListener('click', () => {
-    display.textContent = display.textContent + ' / ';
+    cuentaContainer.textContent = cuentaContainer.textContent + ' / ';
 })
 const btnMultiply = document.querySelector('#btnMultiply');
 btnMultiply.addEventListener('click', () => {
-    display.textContent = display.textContent + ' * ';
+    cuentaContainer.textContent = cuentaContainer.textContent + ' * ';
 })
 const btnSubstract = document.querySelector('#btnSubstract');
 btnSubstract.addEventListener('click', () => {
-    display.textContent = display.textContent + ' - ';
+    cuentaContainer.textContent = cuentaContainer.textContent + ' - ';
 })
 const btnAdd = document.querySelector('#btnAdd');
 btnAdd.addEventListener('click', () => {
-    display.textContent = display.textContent + ' + ';
+    cuentaContainer.textContent = cuentaContainer.textContent + ' + ';
 })
 const btnComma = document.querySelector('#btnComma');
 btnComma.addEventListener('click', () => {
-    display.textContent = display.textContent + ',';
+    cuentaContainer.textContent = cuentaContainer.textContent + ',';
 })
 const btnClear = document.querySelector('#btnClear');
 btnClear.addEventListener('click', () => {
-    display.textContent = '';
+    cuentaContainer.textContent = '';
 })
 const btnEqual = document.querySelector('#btnEqual');
 btnEqual.addEventListener('click', () => {
     
     
-    let a = parseInt(display.textContent.split(' ')[0]);
-    let operator = display.textContent.split(' ')[1];
-    let b = parseInt(display.textContent.split(' ')[2]);
-    display.textContent = operate(a, operator, b);
+    let a = parseInt(cuentaContainer.textContent.split(' ')[0]);
+    let operator = cuentaContainer.textContent.split(' ')[1];
+    let b = parseInt(cuentaContainer.textContent.split(' ')[2]);
+    cuentaContainer.textContent = operate(a, operator, b);
     //console.log(a, operator, b);
-    //console.log(display.textContent.split(' '));
+    //console.log(cuentaContainer.textContent.split(' '));
 })
